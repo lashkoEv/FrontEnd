c6d738102f9d8c6a43720f8134ebff56e420f1e1
Евгения Лашко <smevlk2014@gmail.com>
2023-03-06 20:55:52 +0200

    done practice

--- Lessons/lesson-11-JS/app.js
+++ Lessons/lesson-11-JS/app.js
@@ -1,1 +1,34 @@
 "use strict";
+
+class Hero {
+  fullName;
+  login;
+  password;
+
+  constructor(fullName, login, password) {
+    this.fullName = fullName;
+    this.login = login;
+    this.password = password;
+  }
+
+  signUp() {
+    console.log(`Signed up: ${this.login}`);
+  }
+}
+
+class Wizard extends Hero {
+  constructor(fullName, login, password) {
+    super(fullName, login, password);
+  }
+
+  attack() {
+    console.log("There was an attack!");
+  }
+}
+
+const wizard = new Wizard("John", "john12234", "2323443");
+
+console.log(wizard);
+
+wizard.signUp();
+wizard.attack();
