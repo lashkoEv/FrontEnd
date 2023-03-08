9d19e398c1448afbd19e1fcc122a99dd47e130a6
Евгения Лашко <smevlk2014@gmail.com>
2023-03-08 18:46:03 +0200

    practice init

--- Lessons/lesson-11-JS/app.js
+++ Lessons/lesson-11-JS/app.js
@@ -1,18 +1,33 @@
 "use strict";
 
 class Hero {
-  fullName;
-  login;
-  password;
-
-  constructor(fullName, login, password) {
+  constructor(fullName, login, password, image, positionX, positionY) {
+    this.image = image;
     this.fullName = fullName;
     this.login = login;
     this.password = password;
+    this.positionX = positionX;
+    this.positionY = positionY;
   }
 
   signUp() {
     console.log(`Signed up: ${this.login}`);
+  }
+
+  static goUp() {
+    this.positionX += 1;
+  }
+
+  static goDown() {
+    this.positionX -= 1;
+  }
+
+  static turnRight() {
+    this.positionY += 1;
+  }
+
+  static turnLeft() {
+    this.positionY -= 1;
   }
 }
 
@@ -26,9 +41,17 @@ class Wizard extends Hero {
   }
 }
 
-const wizard = new Wizard("John", "john12234", "2323443");
+const wizard = new Wizard("John", "john12234", "2323443", 50, 50);
 
 console.log(wizard);
 
 wizard.signUp();
 wizard.attack();
+
+const app = document.getElementById("app");
+
+let img = document.createElement("img");
+img.src = "https://www.pngall.com/wp-content/uploads/2016/06/Wizard.png";
+
+
+app.appendChild(img);
