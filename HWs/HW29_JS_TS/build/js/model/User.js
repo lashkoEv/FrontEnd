b6283b9a6e38c2c1d03bca62bcae1cb118b96595
Евгения Лашко <smevlk2014@gmail.com>
2023-03-18 15:56:03 +0200

    add simple User entity

--- HWs/HW29_JS_TS/build/js/model/User.js
+++ HWs/HW29_JS_TS/build/js/model/User.js
@@ -1,3 +1,55 @@
 export default class User {
-  constructor() {}
+  #firstName;
+  #lastName;
+  #age;
+  #email;
+  #password;
+
+  constructor(firstName, lastName, age, email, password) {
+    this.#firstName = firstName;
+    this.#lastName = lastName;
+    this.#age = age;
+    this.#email = email;
+    this.#password = password;
+  }
+
+  get firstName() {
+    return this.#firstName;
+  }
+
+  set firstName(firstName) {
+    this.#firstName = firstName;
+  }
+
+  get lastName() {
+    return this.#lastName;
+  }
+
+  set lastName(lastName) {
+    this.#lastName = lastName;
+  }
+
+  get age() {
+    return this.#age;
+  }
+
+  set age(age) {
+    this.#age = age;
+  }
+
+  get email() {
+    return this.#email;
+  }
+
+  set email(email) {
+    this.#email = email;
+  }
+
+  get password() {
+    return this.#password;
+  }
+
+  set password(password) {
+    this.#password = password;
+  }
 }
