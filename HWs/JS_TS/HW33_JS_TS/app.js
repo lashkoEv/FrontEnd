f834fdfc4cbcaafc5dbd5322e5d0bad95b6806d9
Евгения Лашко <smevlk2014@gmail.com>
2023-03-25 19:13:18 +0200

    hw 33 init

---
+++ HWs/JS_TS/HW33_JS_TS/app.js
@@ -0,0 +1,5 @@
+// 1) Write what do you think about every SOLI'[D] principle 
+// 2) By your suggestion -> write what does mean every SOLI'[D] principle  
+// 3) Wtire your own example by every SOLI'[D] principle ! 
+
+// ;)
