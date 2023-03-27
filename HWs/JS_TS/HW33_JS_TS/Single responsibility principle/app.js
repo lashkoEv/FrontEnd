d3b6df986d1681b19b06d31627ab31fa3283838e
Евгения Лашко <smevlk2014@gmail.com>
2023-03-28 01:38:35 +0300

    fix alignment

--- HWs/JS_TS/HW33_JS_TS/Single responsibility principle/app.js
+++ HWs/JS_TS/HW33_JS_TS/Single responsibility principle/app.js
@@ -1,31 +1,50 @@
 /*
-*   The Book class is responsible for information about the book.
-*   The BookService class is responsible for the CRUD operations with books.
-*/
-
+ *   The Book class is responsible for information about the book.
+ *   The BookService class is responsible for the CRUD operations with books.
+ */
 
 import { Book } from "./components/Book/Book.js";
 import { BookService } from "./components/BookService/BookService.js";
 
 const app = () => {
-    const bookService = new BookService();
+  const bookService = new BookService();
 
-    bookService.save(new Book("Harry Potter and the Prisoner of Azkaban", "J.K. Rowling", " Scholastic Inc.", 100));
-    bookService.save(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", " Scholastic Inc.", 85));
-    console.table(bookService.getAll());
-    
-    const foundBook = bookService.findByTitle("Harry Potter and the Philosopher's Stone");
-    console.log("[FOUND BOOK]" , foundBook);
+  bookService.save(
+    new Book(
+      "Harry Potter and the Prisoner of Azkaban",
+      "J.K. Rowling",
+      " Scholastic Inc.",
+      100
+    )
+  );
+  bookService.save(
+    new Book(
+      "Harry Potter and the Philosopher's Stone",
+      "J.K. Rowling",
+      " Scholastic Inc.",
+      85
+    )
+  );
+  console.table(bookService.getAll());
 
-    bookService.setPrice("Harry Potter and the Philosopher's Stone", 300);
-    const bookAfterUpdating = bookService.findByTitle("Harry Potter and the Philosopher's Stone");
-    console.log("[BOOK AFTER UPDATING]" , bookAfterUpdating);
+  const foundBook = bookService.findByTitle(
+    "Harry Potter and the Philosopher's Stone"
+  );
+  console.log("[FOUND BOOK]", foundBook);
 
-    bookService.delete("Harry Potter and the Philosopher's Stone");
-    const bookAfterDeleting = bookService.findByTitle("Harry Potter and the Philosopher's Stone");
-    console.log("[BOOK AFTER DELETING]" , bookAfterDeleting);
-    
-    console.table(bookService.getAll());
-}
+  bookService.setPrice("Harry Potter and the Philosopher's Stone", 300);
+  const bookAfterUpdating = bookService.findByTitle(
+    "Harry Potter and the Philosopher's Stone"
+  );
+  console.log("[BOOK AFTER UPDATING]", bookAfterUpdating);
+
+  bookService.delete("Harry Potter and the Philosopher's Stone");
+  const bookAfterDeleting = bookService.findByTitle(
+    "Harry Potter and the Philosopher's Stone"
+  );
+  console.log("[BOOK AFTER DELETING]", bookAfterDeleting);
+
+  console.table(bookService.getAll());
+};
 
 app();
