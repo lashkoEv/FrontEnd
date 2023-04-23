87bc162646793a3a6cc3fdb2265f6039743e21c0
Евгения Лашко <smevlk2014@gmail.com>
2023-04-23 21:04:14 +0300

    add character class

---
+++ HWs/JS_TS/HW40_JS_TS/js/Character.js
@@ -0,0 +1,98 @@
+export class Character {
+  #ctx;
+
+  #image;
+
+  #width;
+  #height;
+
+  #positionX;
+  #positionY;
+
+  #isMoveUp;
+  #animationId;
+
+  constructor(ctx) {
+    this.#ctx = ctx;
+
+    this.#width = 200;
+    this.#height = 85;
+
+    this.#positionX = 0;
+    this.#positionY = 0;
+
+    this.#image = new Image();
+    this.#image.src = "./public/images/character/character.png";
+    this.#image.onload = () => {
+      ctx.drawImage(
+        this.#image,
+        this.#positionX,
+        this.#positionY,
+        this.#width,
+        this.#height
+      );
+    };
+
+    this.#isMoveUp = false;
+    this.#animationId = 0;
+
+    this.#setMovement();
+  }
+
+  draw = () => {
+    this.#ctx.drawImage(
+      this.#image,
+      this.#positionX,
+      this.#positionY,
+      this.#width,
+      this.#height
+    );
+  };
+
+  #move = () => {
+    if (this.#isMoveUp) {
+      this.#positionY += 3;
+    } else {
+      this.#positionY -= 3;
+    }
+
+    this.#animationId = requestAnimationFrame(this.#move);
+  };
+
+  #onKeyDown = ({ code }) => {
+    if (code === "ArrowDown") {
+      this.#isMoveUp = true;
+      this.#animationId = requestAnimationFrame(this.#move);
+    }
+
+    if (code === "ArrowUp") {
+      this.#isMoveUp = false;
+      this.#animationId = requestAnimationFrame(this.#move);
+    }
+  };
+
+  #onKeyUp = ({ code }) => {
+    if (code === "ArrowDown") {
+      cancelAnimationFrame(this.#animationId);
+      document.addEventListener("keydown", this.#onKeyDown, { once: true });
+    }
+
+    if (code === "ArrowUp") {
+      cancelAnimationFrame(this.#animationId);
+      document.addEventListener("keydown", this.#onKeyDown, { once: true });
+    }
+  };
+
+  #setMovement() {
+    document.addEventListener("keydown", this.#onKeyDown, { once: true });
+    document.addEventListener("keyup", this.#onKeyUp);
+  }
+
+  get positionX() {
+    return this.#positionX;
+  }
+
+  get positionY() {
+    return this.#positionY;
+  }
+}
