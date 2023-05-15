33923d834c52f6cc70a704a853211dfb9e6c8362
Евгения Лашко <smevlk2014@gmail.com>
2023-05-15 21:16:14 +0300

    hw 43 done

--- HWs/JS_TS/HW43_JS_TS/app.js
+++ HWs/JS_TS/HW43_JS_TS/app.js
@@ -1,97 +1,147 @@
-let actions = [];
+let callbacks = [];
 let count = 1;
 
-const createAction = (classes, actionName) => {
-  const actionElement = document.createElement("div");
+const createElement = (tag, classes, src, textContent) => {
+  const element = document.createElement(tag);
 
   classes.forEach((item) => {
-    actionElement.classList.add(item);
+    element.classList.add(item);
   });
 
-  const actionNameElement = document.createElement("div");
-  actionNameElement.classList.add("name");
-  actionElement.append(actionNameElement);
-
-  if (actionName) {
-    actionNameElement.textContent = actionName;
+  if (textContent) {
+    element.textContent = textContent;
   }
 
-  return actionElement;
+  if (src) {
+    element.src = src;
+  }
+
+  return element;
 };
 
-const initListeners = () => {
+const createCallbackElement = (classes, name, withImg) => {
+  const callbackElement = createElement("div", classes, null, null);
+
+  const callbackNameElement = createElement("div", ["name"], null, name);
+
+  callbackElement.append(callbackNameElement);
+
+  if (withImg) {
+    const img = createElement(
+      "img",
+      ["img"],
+      "./public/img/processing2.gif",
+      null
+    );
+
+    callbackElement.append(img);
+  }
+
+  return callbackElement;
+};
+
+const getName = () => {
+  let name = `Callback${count++}`;
+
+  if (callbackName.value.trim().length !== 0) {
+    name = callbackName.value.trim();
+  }
+
+  return name;
+};
+
+const getTime = () => {
+  let time = 1000;
+
+  if (callbackTime.value > 1000) {
+    time = +callbackTime.value;
+  }
+
+  return time;
+};
+
+const getCallback = () => {
+  const callback = {
+    name: getName(),
+    time: getTime(),
+  };
+
+  callbacks.push(callback);
+
+  return callback;
+};
+
+const initAddCallbackListener = () => {
   addCallback.addEventListener("click", () => {
-    let name = `Callback${count}`;
-    count++;
+    const { name } = getCallback();
 
-    if (callbackName.value.trim().length !== 0) {
-      name = callbackName.value.trim();
-    }
-
-    let time = 1000;
-
-    if (callbackTime.value > 1000) {
-      time = +callbackTime.value;
-    }
-
-    const element = createAction(["action", "event-action"], name);
+    const element = createCallbackElement(["callback", "event-callback"], name);
     eventQueue.append(element);
 
     element.animate(
       [{ transform: "translateX(40px)" }, { transform: "translateX(0px)" }],
       300
     );
-
-    actions.push({ name, time });
-    console.log(callStack.children.length);
-    console.log(eventQueue.children.length);
   });
+};
+
+const addCallbackToCallStack = (name) => {
+  const htmlElement = createCallbackElement(["callback"], name, true);
+  callStack.append(htmlElement);
+
+  htmlElement.animate(
+    [{ transform: "translateY(-40px)" }, { transform: "translateY(0px)" }],
+    300
+  );
+
+  return htmlElement;
+};
+
+const removeCallbackFromEventQueue = () => {
+  eventQueue.children[0].animate(
+    [{ transform: "translateX(0px)" }, { transform: "translateX(-40px)" }],
+    300
+  );
+
+  setTimeout(() => {
+    eventQueue.removeChild(eventQueue.children[0]);
+  }, 200);
+};
+
+const removeCallbackFromCallStack = (time, htmlElement) => {
+  setTimeout(() => {
+    htmlElement.animate(
+      [
+        { transform: "translateY(0px)" },
+        { transform: "translateY(-40px)" },
+        { opacity: 0 },
+      ],
+      300
+    );
+
+    setTimeout(() => {
+      callStack.removeChild(callStack.children[0]);
+    }, 200);
+  }, time);
 };
 
 const checkCallStack = () => {
   setInterval(() => {
-    if (callStack.children.length === 0 && actions.length > 0) {
-      const element = actions.shift();
+    if (callStack.children.length === 0 && callbacks.length > 0) {
+      const element = callbacks.shift();
 
-      const htmlElement = createAction(["action"], element.name);
-      callStack.append(htmlElement);
+      removeCallbackFromEventQueue();
 
-      htmlElement.animate(
-        [{ transform: "translateY(-40px)" }, { transform: "translateY(0px)" }],
-        300
-      );
+      const htmlElement = addCallbackToCallStack(element.name);
 
-      eventQueue.children[0].animate(
-        [{ transform: "translateX(0px)" }, { transform: "translateX(-40px)" }],
-        300
-      );
-
-      setTimeout(() => {
-        eventQueue.removeChild(eventQueue.children[0]);
-      }, 200);
-
-      setTimeout(() => {
-        htmlElement.animate(
-          [
-            { transform: "translateY(0px)" },
-            { transform: "translateY(-40px)" },
-            { opacity: 0 },
-          ],
-          300
-        );
-
-        setTimeout(() => {
-          callStack.removeChild(callStack.children[0]);
-        }, 200);
-      }, element.time);
+      removeCallbackFromCallStack(element.time, htmlElement);
     }
   }, 1000);
 };
 
 const app = () => {
-  initListeners();
+  initAddCallbackListener();
   checkCallStack();
 };
 
 app();
-
