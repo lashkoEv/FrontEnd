ab899e097e9d9270de166adc3a8a86c0505f17ed
Евгения Лашко <smevlk2014@gmail.com>
2023-10-22 07:52:19 +0300

    refactor requests

--- HWs/JS_TS_NEW/HW50_JS_TS/store/requests.js
+++ HWs/JS_TS_NEW/HW50_JS_TS/store/requests.js
@@ -1,11 +1,21 @@
 export const mainMenuRequest = `Welcome to the Shop!\n1) View products\n2) Set filters\n3) Sort products\n4) Exit`;
+
 export const criterionRequest = `Select sorting criterion:\n1) Category\n2) Price\n3) Manufacturer\n4) Date of manufacture\n5) Delete sorting`;
+
 export const directionRequest = `Select sorting direction:\n1) Asc\n2) Desc`;
+
 export const categoryRequest = `a) TV\nb) Laptop\nc) Smartphone\nd) Fridge\ne) Boiler\nf) Stove\ng) Washing Machine\nh) Vacuum Cleaner\ni) Conditioner\nj) Iron\nk) Teapot\nl) Electric Shaver\nm) Toaster\nn) Coffee Machine`;
+
 export const minPriceRequest = `Enter a min price:`;
+
 export const maxPriceRequest = `Enter a max price:`;
+
 export const minDateRequest = `Enter a min date (month.year):`;
+
 export const maxDateRequest = `Enter a max date (month.year):`;
+
 export const manufacturerRequest = `a) Sony\nb) Acer\nc) Apple\nd) Vestfrost\ne) Indesit\nf) Gorenje\ng) Electrolux\nh) Samsung\ni) Toshiba\nj) Philips\nk) Bosch\nl) Braun\nm) Tefal\nn) Delonghi`;
+
 export const filterRequest = `Select a filter to install:\n1) Category\n2) Price\n3) Manufacturer\n4) Date of manufacture\n5) Delete filters\n6) Back to menu`;
+
 export const catManMenuRequest = `1) Only show...\n2) Show everything except...`;
