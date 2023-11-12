5cc80be1014529a8b11d69c6a36d3a33601d8c57
Евгения Лашко <smevlk2014@gmail.com>
2023-11-13 00:43:22 +0200

    swap elements

--- HWs/JS_TS_NEW/HW54_JS_TS/apps/Game.js
+++ HWs/JS_TS_NEW/HW54_JS_TS/apps/Game.js
@@ -12,7 +12,52 @@ export class Game {
     this.#scene = new Scene(generateColors());
 
     render(app, [this.#bar.outerComponent, this.#scene.component]);
+
+    console.log(this.#scene.matrix);
+    this.#setBlocksListeners();
   }
 
-  
+  #setBlocksListeners() {
+    for (let i = 0; i < this.#scene.matrix.length; i++) {
+      for (let j = 0; j < this.#scene.matrix[i].length; j++) {
+        this.#scene.matrix[i][j].component.addEventListener("click", () => {
+          if (this.#scene.matrix[i][j].color === 0) {
+            return;
+          }
+
+          if (i - 1 >= 0) {
+            if (this.#scene.matrix[i - 1][j].color === 0) {
+              this.#swap(i, j, i - 1, j);
+            }
+          }
+
+          if (i + 1 < 4) {
+            if (this.#scene.matrix[i + 1][j].color === 0) {
+              this.#swap(i, j, i + 1, j);
+            }
+          }
+
+          if (j - 1 >= 0) {
+            if (this.#scene.matrix[i][j - 1].color === 0) {
+              this.#swap(i, j, i, j - 1);
+            }
+          }
+
+          if (j + 1 < 4) {
+            if (this.#scene.matrix[i][j + 1].color === 0) {
+              this.#swap(i, j, i, j + 1);
+            }
+          }
+        });
+      }
+    }
+  }
+
+  #swap(row1, col1, row2, col2) {
+    const tmp = this.#scene.matrix[row1][col1].color;
+
+    this.#scene.matrix[row1][col1].color = this.#scene.matrix[row2][col2].color;
+
+    this.#scene.matrix[row2][col2].color = tmp;
+  }
 }
