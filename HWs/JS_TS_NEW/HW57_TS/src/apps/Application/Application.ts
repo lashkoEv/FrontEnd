2b2b107f9d968d0c157ec0ec6db17e452f218858
Евгения Лашко <smevlk2014@gmail.com>
2023-11-27 01:04:21 +0200

    finish app, hw done

--- HWs/JS_TS_NEW/HW57_TS/src/apps/Application/Application.ts
+++ HWs/JS_TS_NEW/HW57_TS/src/apps/Application/Application.ts
@@ -1,3 +1,48 @@
+import { ModalWindow } from "../../components";
+import { append, render } from "../../core";
+
 export class Application {
-    
+  private app: HTMLElement | null;
+  private modalWindow: ModalWindow;
+  private interval: number | null;
+
+  constructor() {
+    this.interval = null;
+
+    this.app = document.getElementById("app");
+
+    this.modalWindow = new ModalWindow(
+      {
+        click: () => {
+          this.modalWindow.getTimer().reset();
+          this.run();
+        },
+      },
+      {
+        click: () => {
+          alert("Goodbye!");
+          window.close();
+        },
+      }
+    );
+  }
+
+  run() {
+    setTimeout(() => this.showModalWindow(), 10000);
+  }
+
+  private showModalWindow() {
+    if (this.app) render(this.app, this.modalWindow.getComponent());
+
+    if (this.interval) clearInterval(this.interval);
+
+    this.interval = setInterval(() => {
+      const count = this.modalWindow.getTimer().decreaseTime();
+
+      if (count < 0) {
+        alert("R U here?");
+        this.modalWindow.getTimer().reset();
+      }
+    }, 1000);
+  }
 }
