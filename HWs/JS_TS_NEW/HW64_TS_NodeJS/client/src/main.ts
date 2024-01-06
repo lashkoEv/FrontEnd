bf1b08b1d1f2efb37c868c5c0fedfb27b0bcf2da
Евгения Лашко <smevlk2014@gmail.com>
2024-01-06 19:35:15 +0200

    test server

--- HWs/JS_TS_NEW/HW64_TS_NodeJS/client/src/main.ts
+++ HWs/JS_TS_NEW/HW64_TS_NodeJS/client/src/main.ts
@@ -1,1 +1,39 @@
 import "./style.css";
+const app = async () => {
+  // * init user
+  // const f = await fetch("http://localhost:3000/authorization/init");
+
+  // * auth
+  //   const f = await fetch("http://localhost:3000/authorization", {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     method: "POST",
+  //     body: JSON.stringify({
+  //       login: "admin",
+  //       password: "admin",
+  //     }),
+  //   });
+  //   console.log(await f.json());
+
+  // * get all products
+  // const f = await fetch("http://localhost:3000/products");
+  // console.log(await f.json());
+
+  //   * create product
+  //   const f = await fetch("http://localhost:3000/products", {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     method: "POST",
+  //     body: JSON.stringify({
+  //       title: "Product 1",
+  //       description: "req.body.description",
+  //       newPrice: 1,
+  //       oldPrice: 1,
+  //     }),
+  //   });
+  //   console.log(await f.json());
+};
+
+app();
