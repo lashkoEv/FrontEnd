1eb69bc8f8040dcc5881ab1d8f121d42edd62650
Евгения Лашко <smevlk2014@gmail.com>
2024-01-30 18:15:57 +0200

    create components, services, add angular material

---
+++ HWs/frameworks/HW68-Angular/src/app/components/authorization-form/authorization-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authorization-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './authorization-form.component.html',
+  styleUrl: './authorization-form.component.css'
+})
+export class AuthorizationFormComponent {
+
+}
