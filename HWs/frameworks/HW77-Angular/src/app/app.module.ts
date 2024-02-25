8d8b73f63236bb4ab58205088878adce19acb206
Евгения Лашко <smevlk2014@gmail.com>
2024-02-25 17:19:15 +0200

    add material

--- HWs/frameworks/HW77-Angular/src/app/app.module.ts
+++ HWs/frameworks/HW77-Angular/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
