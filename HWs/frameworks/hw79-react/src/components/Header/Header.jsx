c54e3023ef794bbc14cae64b4c1bda634ac2e7c4
Евгения Лашко <smevlk2014@gmail.com>
2024-03-03 15:23:34 +0200

    add header

--- HWs/frameworks/hw79-react/src/components/Header/Header.jsx
+++ HWs/frameworks/hw79-react/src/components/Header/Header.jsx
@@ -1,5 +1,38 @@
 import "./Header.css";
 
+const navLinks = [
+  {
+    title: "Home",
+    href: "#",
+  },
+  {
+    title: "About Us",
+    href: "#",
+  },
+  {
+    title: "Contact Us",
+    href: "#",
+  },
+];
+
 export const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="header">
+      <div className="input-wrapper">
+        <input type="text" className="input" placeholder="Search..." />
+      </div>
+
+      <nav className="navigation">
+        {navLinks.map((item) => (
+          <a href={item.href} className="link">
+            {item.title}
+          </a>
+        ))}
+      </nav>
+
+      <div className="login-wrapper">
+        <button className="login">Login</button>
+      </div>
+    </header>
+  );
 };
