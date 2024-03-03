ba3c7a894d1de1211a81528897782416fa91d69a
Евгения Лашко <smevlk2014@gmail.com>
2024-03-03 22:34:46 +0200

    add footer (create task)

--- HWs/frameworks/HW78-Angular/src/app/components/footer/footer.component.ts
+++ HWs/frameworks/HW78-Angular/src/app/components/footer/footer.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
+  constructor(private taskService: TaskService) {}
 
+  add(data: any) {
+    if (data.title) {
+      this.taskService.add(data.title);
+    }
+  }
 }
