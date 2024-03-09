72c86c1cca25b5d510929ec70cb13e9970113d1f
Евгения Лашко <smevlk2014@gmail.com>
2024-03-09 19:38:56 +0200

    comparison page

--- HWs/frameworks/hw80-react/src/pages/Comparison/Comparison.jsx
+++ HWs/frameworks/hw80-react/src/pages/Comparison/Comparison.jsx
@@ -1,5 +1,19 @@
 import "./Comparison.css";
 
 export const Comparison = () => {
-  return <section className="comparison">comparison</section>;
+  return (
+    <section className="comparison">
+      <div className="column">
+        <h1 className="title">From:</h1>
+
+        <img src="images/photos/album/1.jpg" alt="From" className="image" />
+      </div>
+
+      <div className="column">
+        <h1 className="title">To:</h1>
+
+        <img src="images/photos/album/2.jpg" alt="To" className="image" />
+      </div>
+    </section>
+  );
 };
