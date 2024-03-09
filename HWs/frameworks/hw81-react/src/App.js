c05bbce627c4ba9119b9231d421a9831380569c3
Евгения Лашко <smevlk2014@gmail.com>
2024-03-10 01:17:25 +0200

    hw init

---
+++ HWs/frameworks/hw81-react/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Root />}>
+//       <Route path="dashboard" element={<Dashboard />} />
+//     </Route>
+//   )
+// );
+
+function App() {
+  return <div className="App">{/* <RouterProvider router={router} /> */}</div>;
+}
+
+export default App;
