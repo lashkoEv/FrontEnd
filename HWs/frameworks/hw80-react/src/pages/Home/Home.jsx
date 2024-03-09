d47c7306dbfd679896064309e54e10ad884ead10
Евгения Лашко <smevlk2014@gmail.com>
2024-03-09 20:19:58 +0200

    home page

--- HWs/frameworks/hw80-react/src/pages/Home/Home.jsx
+++ HWs/frameworks/hw80-react/src/pages/Home/Home.jsx
@@ -1,5 +1,36 @@
 import "./Home.css";
 
 export const Home = () => {
-  return <section className="home">home</section>;
+  return (
+    <section className="home">
+      <div className="column">
+        <img src="images/photos/3.jpg" alt="Lisa" className="image" />
+      </div>
+
+      <div className="column">
+        <div className="table">
+          <div className="row">
+            <div className="title">Name:</div>
+            <div className="title">Lisa</div>
+          </div>
+          <div className="row">
+            <div className="text">Breed:</div>
+            <div className="text">British Shorthair</div>
+          </div>
+          <div className="row">
+            <div className="text">Color:</div>
+            <div className="text">NY-11</div>
+          </div>
+          <div className="row">
+            <div className="text">Gender:</div>
+            <div className="text">Female</div>
+          </div>
+          <div className="row">
+            <div className="text">Birth:</div>
+            <div className="text">20.08.2023</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
