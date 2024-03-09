53b125dbebe4c13850d50cb4f74da7f08b6ec8cd
Евгения Лашко <smevlk2014@gmail.com>
2024-03-09 19:20:18 +0200

    error page, add fonts

--- HWs/frameworks/hw80-react/src/pages/Error/Error.jsx
+++ HWs/frameworks/hw80-react/src/pages/Error/Error.jsx
@@ -1,5 +1,10 @@
 import "./Error.css";
 
 export const Error = () => {
-  return <section className="error">error</section>;
+  return (
+    <section className="error">
+      <h1 className="title">Woops!</h1>
+      <h3>Something went wrong...</h3>
+    </section>
+  );
 };
