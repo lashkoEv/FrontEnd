700c78d4220460c7201d03b4bf753dac15267b61
Евгения Лашко <smevlk2014@gmail.com>
2024-03-16 22:19:20 +0200

    hw done

--- HWs/frameworks/hw81-react/src/components/Product/Product.jsx
+++ HWs/frameworks/hw81-react/src/components/Product/Product.jsx
@@ -10,13 +10,17 @@ export const Product = ({ data, onClickHandler, text, inCart }) => {
   return (
     <div className="p-6 rounded-2xl bg-slate-600 flex flex-col gap-4 relative">
       <img src={data.src} alt={data.title} />
+
       <div className="flex justify-end absolute top-6 right-3">
         <span className="bg-lime-600 px-3 py-1">{data.category}</span>
       </div>
+
       <div className="flex w-full justify-between py-6 items-center flex-wrap">
         <h3 className="text-l">{data.title}</h3>
+
         <h3 className="text-xl">{data.price} $</h3>
       </div>
+
       {button}
     </div>
   );
--- HWs/frameworks/hw81-react/src/pages/ErrorPage/ErrorPage.jsx
+++ HWs/frameworks/hw81-react/src/pages/ErrorPage/ErrorPage.jsx
@@ -2,6 +2,7 @@ export const ErrorPage = () => {
   return (
     <>
       <h1 className="text-5xl p-6">Woops!</h1>
+
       <h3 className="text-2xl p-6">Something went wrong...</h3>
     </>
   );
