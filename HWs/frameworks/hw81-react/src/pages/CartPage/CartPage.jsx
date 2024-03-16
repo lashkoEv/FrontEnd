fbb0e1b3a7646080daae55427f560f600fae0998
Евгения Лашко <smevlk2014@gmail.com>
2024-03-16 22:15:23 +0200

    add "remove from cart" logic

--- HWs/frameworks/hw81-react/src/pages/CartPage/CartPage.jsx
+++ HWs/frameworks/hw81-react/src/pages/CartPage/CartPage.jsx
@@ -1,14 +1,26 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ProductContext } from "../../contexts";
 import { Product } from "../../components";
 
 export const CartPage = () => {
   const products = useContext(ProductContext);
 
+  const [toShow, setToShow] = useState(products.inCart);
+
+  function onRemoveHandler(product) {
+    products.inCart = products.inCart.filter((p) => p !== product);
+    setToShow(products.inCart);
+  }
+
   return (
     <div className="py-6 px-64 grid grid-cols-4 gap-8">
-      {useContext(ProductContext).inCart.map((product, i) => (
-        <Product key={i} data={product} inCart={true} />
+      {toShow.map((product, i) => (
+        <Product
+          key={i}
+          data={product}
+          onClickHandler={() => onRemoveHandler(product)}
+          text={"Remove from cart"}
+        />
       ))}
     </div>
   );
