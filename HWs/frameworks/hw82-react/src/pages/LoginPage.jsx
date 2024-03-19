8525d57945b33a31b7409de389899be99737b7f3
Евгения Лашко <smevlk2014@gmail.com>
2024-03-20 00:04:40 +0200

    hw done

--- HWs/frameworks/hw82-react/src/pages/LoginPage.jsx
+++ HWs/frameworks/hw82-react/src/pages/LoginPage.jsx
@@ -38,6 +38,7 @@ export const LoginPage = () => {
           >
             Your email
           </label>
+
           <input
             type="email"
             id="email"
@@ -47,6 +48,7 @@ export const LoginPage = () => {
             onInput={(e) => dispatch({ type: "email", e: e })}
           />
         </div>
+
         <div className="mb-5">
           <label
             htmlFor="password"
@@ -54,6 +56,7 @@ export const LoginPage = () => {
           >
             Your password
           </label>
+
           <input
             type="password"
             id="password"
@@ -62,6 +65,7 @@ export const LoginPage = () => {
             onInput={(e) => dispatch({ type: "password", e: e })}
           />
         </div>
+
         <div className="flex items-start mb-5">
           <div className="flex items-center h-5">
             <input
@@ -72,6 +76,7 @@ export const LoginPage = () => {
               required
             />
           </div>
+
           <label
             htmlFor="remember"
             className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
@@ -79,6 +84,7 @@ export const LoginPage = () => {
             Remember me
           </label>
         </div>
+
         <button
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
